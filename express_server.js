8a1d5e5f67cceca9559f350995c6fa7225a4077d
GraceLR <gracerwork08@gmail.com>
2021-12-07 23:27:21 +0000

    adding cookie

--- express_server.js
+++ express_server.js
@@ -11,7 +11,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+var cookieParser = require('cookie-parser');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
@@ -33,12 +35,18 @@ res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get("/urls", (req, res) => {
-    const templateVars = { urls: urlDatabase };
+    const templateVars = {
+        username: req.cookies["username"],
+        urls: urlDatabase
+      };
     res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
+    const templateVars = {
+        username: req.cookies["username"],
+      };
+    res.render("urls_new", templateVars);
   });
 
 app.post("/urls", (req, res) => {
@@ -53,22 +61,35 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-    const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+    const templateVars = { 
+        username: req.cookies["username"],
+        shortURL: req.params.shortURL, 
+        longURL: urlDatabase[req.params.shortURL] };
     res.render("urls_show", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
+    const templateVars = { username: req.cookies["username"] };
     const shortURL = req.params.shortURL;
     const longURL = urlDatabase[shortURL];
     if (longURL === undefined) {
-        return res.render("u_index");
+        return res.render("u_index", templateVars);
     }
     res.redirect(longURL);
   });
 
 app.post("/urls/:id", (req, res) => {
-    console.log('http://' + req.body.UpdatedlongURL);
-    // link to shortURL, change database
+    urlDatabase[req.params.id] = 'http://' + req.body.newURL;
+    res.redirect(`/urls`);
+});
+
+app.post("/login", (req, res) => {
+    res.cookie('username', req.body.username);
+    res.redirect(`/urls`);
+});
+
+app.post("/logout", (req, res) => {
+    res.clearCookie('username')
     res.redirect(`/urls`);
 });
 
